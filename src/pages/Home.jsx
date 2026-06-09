076ef097f38676554bd18125971a3151db48c85e
gustavoweb <contato@gustavoweb.com>
2026-06-09 11:46:07 -0300

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,23 @@
+import { BrowserRouter, Link } from 'react-router-dom'
 import styles from './Home.module.css'
 import homeGif from '../assets/home.gif'
+import Sobre from './Sobre'
+import Contato from './Contato'
 
 function Home() {
   return (
     <div className={styles.home}>
       <h1>Home</h1>
-      <img src={homeGif} alt="" />
-      <button>Saiba +</button>
-      <button>Contato</button>
+      <img src={homeGif} alt="" className={styles.gif}/>
+      <BrowserRouter>
+        <Link to="/sobre">
+          <button>Saiba +</button>
+        </Link>
+        
+        <Link to="/sobre">
+          <button>Contato</button>
+        </Link>
+      </BrowserRouter>
     </div>
   )
 }
