47e6c93f14cf48f4d8bf01a3c963aab4016b433c
gustavoweb <contato@gustavoweb.com>
2026-06-09 09:44:54 -0300

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import styles from './Home.module.css'
+import homeGif from '../assets/home.gif'
+
+function Home() {
+  return (
+    <div className={styles.home}>
+      <h1>Home</h1>
+      <img src={homeGif} alt="" />
+      <button>Saiba +</button>
+      <button>Contato</button>
+    </div>
+  )
+}
+
+export default Home
